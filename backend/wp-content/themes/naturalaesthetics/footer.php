c7864bd8da4fb3cf96b2bf28265bf83d2bde1b48
Andy Ettisberger <andy.ettisberger@youengineering.com>
2016-06-12 01:23:00 +0200

    Restructure backend and adjust gitconfig

---
+++ backend/wp-content/themes/naturalaesthetics/footer.php
@@ -0,0 +1,7 @@
+    <footer>
+
+    </footer>
+    <?php wp_footer(); ?>
+    <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/naturalaesthetics.js"></script>
+    </body>
+</html>
---
+++ backend/wp-content/themes/naturalaesthetics/includes/utils/customfield.util.php
@@ -0,0 +1,57 @@
+<?php
+
+class CustomFieldUtils {
+
+    function __construct() {
+        if( function_exists('acf_add_options_page') ) {
+            $this->registerOptionPages();
+        }
+    }
+
+    function registerOptionPages() {
+        acf_add_options_page(array(
+            'page_title' 	=> 'Stammdaten',
+            'menu_title'	=> 'Stammdaten',
+            'menu_slug' 	=> 'naturalaesthetics-general-settings',
+            'capability'	=> 'edit_posts',
+            'redirect'		=> true,
+            'icon_url'      => 'dashicons-networking'
+        ));
+
+        acf_add_options_sub_page(array(
+            'page_title' 	=> 'Produkte',
+            'menu_title'	=> 'Produkte',
+            'menu_slug' 	=> 'naturalaesthetics-products',
+            'capability'	=> 'edit_posts',
+            'parent_slug'	=> 'naturalaesthetics-general-settings'
+        ));
+
+        acf_add_options_sub_page(array(
+            'page_title' 	=> 'Links',
+            'menu_title'	=> 'Links',
+            'menu_slug' 	=> 'naturalaesthetics-links',
+            'capability'	=> 'edit_posts',
+            'parent_slug'	=> 'naturalaesthetics-general-settings'
+        ));
+
+        acf_add_options_page(array(
+            'page_title' 	=> 'Layout',
+            'menu_title'	=> 'Layout',
+            'menu_slug' 	=> 'naturalaesthetics-layout-settings',
+            'capability'	=> 'edit_posts',
+            'redirect'		=> true,
+            'icon_url'      => 'dashicons-id'
+        ));
+
+        acf_add_options_sub_page(array(
+            'page_title' 	=> 'Fusszeile',
+            'menu_title'	=> 'Fusszeile',
+            'menu_slug' 	=> 'naturalaesthetics-footer-settings',
+            'capability'	=> 'edit_posts',
+            'parent_slug'	=> 'naturalaesthetics-layout-settings'
+        ));
+
+    }
+}
+
+?>
